bc9b1dddd7f7f9737f3e05713fab4c52c1ab2f4f
soma izumi <106084178+izumi9708@users.noreply.github.com>
2023-04-04 23:05:12 +0900

    Update index.ts

--- index.ts
+++ index.ts
@@ -104,3 +104,162 @@ console.log(stack.pop()); // 3
 console.log(stack.pop()); // 2
 console.log(stack.pop()); // 1
 console.log(stack.isEmpty()); // true
+
+
+// 以下の要件を満たすTodoListクラスをTypeScriptで実装してください。
+
+// TodoItemインターフェースを作成し、以下のプロパティを持つ
+// title: string型
+// description: string型
+// completed: boolean型
+// TodoListクラスを作成し、以下のプライベートプロパティを持つ
+// todoItems: TodoItem型の配列
+// TodoListクラスに以下のパブリックメソッドを追加する
+// addTodoItem(title: string, description: string): void
+// 引数のtitleとdescriptionを元に新しいTodoItemを作成し、todoItems配列に追加する
+// toggleTodoItemStatus(title: string): void
+// 引数のtitleに該当するTodoItemのcompletedプロパティを反転する
+// deleteTodoItem(title: string): void
+// 引数のtitleに該当するTodoItemをtodoItems配列から削除する
+// getTodoItems(): TodoItem[]
+// todoItems配列を返す
+// 作成したTodoListクラスを使用し、以下の処理を行う
+// TodoListのインスタンスを作成する
+// addTodoItemメソッドを使用し、3つのTodoItemを追加する
+// getTodoItemsメソッドを使用し、現在のTodoItem一覧を取得して表示する
+// toggleTodoItemStatusメソッドを使用し、追加したTodoItemのcompletedプロパティを反転させる
+// getTodoItemsメソッドを使用し、現在のTodoItem一覧を取得して表示する
+// deleteTodoItemメソッドを使用し、2つ目のTodoItemを削除する
+// getTodoItemsメソッドを使用し、現在のTodoItem一覧を取得して表示する
+
+interface TodoItem {
+  title:string
+  description:string
+  completed:boolean
+}
+
+class TodoList{
+  private todoItems:TodoItem[] = [];
+
+  public addTodoItem(title: string, description: string):void{
+    const newArray = {title:title,description:description,completed:false};
+
+    this.todoItems.push(newArray);
+  }
+
+  public toggleTodoItemStatus(title:string):void{
+    this.todoItems.filter((a:TodoItem) => a.title == title ? a.completed = !a.completed : a.completed)
+  }
+
+  public deleteTodoItem(title:string):void{
+    this.todoItems = this.todoItems.filter((a:TodoItem) => a.title !== title)
+  }
+
+  public getTodoItems():TodoItem[]{
+    return this.todoItems;
+  }
+
+}
+
+const list = new TodoList();
+list.addTodoItem('タスク１','課題を終わらせる')
+list.addTodoItem('タスク２','ご飯を食べる')
+list.addTodoItem('タスク３','掃除をする')
+
+console.log(list.getTodoItems());
+
+list.toggleTodoItemStatus('タスク３')
+
+console.log(list.getTodoItems())
+list.deleteTodoItem('タスク２')
+console.log(list.getTodoItems())
+
+
+
+
+// 以下のクラス定義に対して、addStudent メソッドを追加し、students 配列に新しい Student オブジェクトを追加できるようにしてください。Student クラスは既に定義済みとします。
+
+
+class Student {
+  private name: string;
+  private age: number;
+
+  constructor(name: string, age: number) {
+    this.name = name;
+    this.age = age;
+  }
+
+  getName(): string {
+    return this.name;
+  }
+
+  getAge(): number {
+    return this.age;
+  }
+}
+
+class School {
+  private students: Student[];
+
+  constructor() {
+    this.students = [];
+  }
+
+  // ここに addStudent メソッドを実装してください
+  addStudent(age:number,name:string):void{
+    const newObj = new Student(name,age);
+    this.students.push(newObj);
+
+  }
+
+  getStudents(): Student[] {
+    return this.students;
+  }
+}
+
+
+const student = new Student('mike',10);
+console.log(student)
+const school = new School();
+school.addStudent(student.getAge(),student.getName());
+
+
+// 1 ジェネリクスの使い方を説明
+
+// 使用する際に型を指定することができる
+function sample<T>(a:T){
+  return a;
+}
+
+sample<number>(1)
+
+// ２ 継承の使い方を説明
+class Parent {
+  public name:string;
+  public age :number;
+
+  constructor(name:string,age:number){
+    this.name = name;
+    this.age = age;
+  }
+}
+
+class Child extends Parent {
+  public gender:string
+
+  constructor(name:string,age:number,gender:string){
+    super(name,age);
+      this.gender = gender;
+  }
+
+  display(){
+    const {name,age,gender} = this;
+    console.log(name,age,gender);
+  }
+}
+
+const classChild  = new Child('satoshi',5,'mens');
+classChild.display()
+
+
+
