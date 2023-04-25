fb1723988c21780fe7bc7cd7669b671118af65e2
soma izumi <106084178+izumi9708@users.noreply.github.com>
2023-04-25 22:00:41 +0900

    Update index_5.ts

--- index_5.ts
+++ index_5.ts
@@ -70,3 +70,184 @@ const ageByGender = averageAgeByGender(genderArray);
 for(let i of ageByGender){
   console.log(i)
 }
+
+
+// 以下のコードで、Person クラスを継承して Employee クラスを定義してください。Employee クラスには id プロパティと role プロパティを追加してください。
+
+class Person {
+  protected name: string;
+
+  constructor(name: string) {
+    this.name = name;
+  }
+
+  returnName(){
+    return this.name;
+  }
+
+  sayHello() {
+    console.log(`Hello, my name is ${this.name}`);
+  }
+}
+
+// ここにEmployeeクラスを定義してください
+class Employee extends Person {
+  public id:number
+  public role:string
+
+  constructor(name:string,id:number,role:string){
+    super(name)
+    this.id   = id;
+    this.role = role;
+  }
+}
+
+const employee = new Employee("John Smith", 1, "developer");
+// console.log('name',employee.returnName()); // => "John Smith"
+// console.log('id',employee.id); // => 1
+// console.log(employee.role); // => "developer"
+// employee.sayHello(); // => "Hello, my name is John Smith"
+
+
+// 以下のコードで定義された groupBy 関数を実装してください。 groupBy 関数は、与えられた配列とキーのプロパティ名に基づいて、オブジェクトの配列をグループ化するものです。
+
+interface Item {
+  id: number;
+  name: string;
+  category: string;
+}
+
+// function groupBy(items: Item[], key: string): { [key: string]: Item[] } {
+//   const newObj = {}
+  
+//   for(let i of items){
+//     newObj[i[key]] = items.filter(val => i[key] == val[key])
+//   }
+
+//   return newObj;
+// }
+
+function groupBy(items: Item[], key: string): { [key: string]: Item[] } {
+  const newObj:{[key:string]:Item[]} = {} 
+
+  for(let item of items){
+    const keys = item[key];
+    
+    if(!newObj[keys]){
+      newObj[keys] = []
+    }
+    newObj[keys].push(item)
+  }
+
+  return newObj
+}
+
+const items: Item[] = [
+  { id: 1, name: 'item 1', category: 'category 1' },
+  { id: 2, name: 'item 2', category: 'category 2' },
+  { id: 3, name: 'item 3', category: 'category 1' },
+  { id: 4, name: 'item 4', category: 'category 2' },
+  { id: 5, name: 'item 5', category: 'category 1' },
+];
+
+const itemsById = groupBy(items, 'id');
+const itemsByCategory = groupBy(items, 'category');
+
+// console.log(itemsById);
+// console.log(itemsByCategory);
+
+
+
+// 配列に重複した要素が含まれている場合に、重複した要素を削除して、新しい配列を作成する関数を実装してください。ただし、新しい配列は元の配列の順序を保持する必要があります。
+
+function removeDuplicates(arr:number[]):number[] {
+    return Array.from(new Set(arr))
+}
+
+const arr = [1, 3, 2, 3, 4, 1, 2];
+const result = removeDuplicates(arr);
+// console.log(result); // [1, 3, 2, 4]
+
+
+// この配列を価格が高い順に並び替え、結果をコンソールに出力する関数 sortFruitsByPrice を実装してください。
+
+const fruits = [
+  { name: 'apple', color: 'red', price: 0.5 },
+  { name: 'banana', color: 'yellow', price: 0.3 },
+  { name: 'grape', color: 'purple', price: 0.8 },
+  { name: 'orange', color: 'orange', price: 0.6 },
+];
+
+interface Fruits {
+  name:string
+  color:string
+  price:number
+}
+
+function sortFruisByPrice(array:Fruits[]):Fruits[]{
+  return array.sort((a,b) => b.price - a.price)
+}
+
+// console.log(sortFruisByPrice(fruits))
+
+
+// Userクラスを定義してください。Userクラスは、名前とメールアドレスをプロパティとして持ちます。また、sayHello()メソッドを定義して、"Hello, {名前}!"というメッセージをコンソールに表示するようにしてください。
+
+class User {
+  private _name: string;
+  private _email: string;
+  constructor(name: string, email: string) {
+    this._name = name;
+    this._email = email;
+  }
+  public get name(): string {
+    return this._name;
+  }
+  public get email(): string {
+    return this._email;
+  }
+  public sayHello(): void {
+    console.log(`Hello, ${this._name}!`);
+  }
+}
+
+const classUser = new User('taro','aaa@mail.com');
+// classUser.sayHello();
+
+
+
+// 次のような型を持つStackクラスを定義してください
+
+class Stack<T> {
+  private items: T[] = [];
+
+  push(item: T): void {
+    this.items.push(item);
+  }
+
+  pop(): T | undefined {
+    return this.items.pop();
+  }
+
+  isEmpty(): boolean {
+    return this.items.length === 0;
+  }
+}
+
+const stack = new Stack<number>();
+
+
+// 次のような型を持つPersonインターフェースがあります。Personインターフェースを拡張して、Studentインターフェースを定義してください。Studentインターフェースは、Personインターフェースを継承します。また、universityプロパティを追加してください。universityプロパティは、大学名の文字列を持ちます。
+
+interface Person {
+  name: string;
+  age: number;
+  gender: 'male' | 'female' | 'other';
+}
+
+interface Student_2 extends Person {
+  university:string
+}
+
+
+
